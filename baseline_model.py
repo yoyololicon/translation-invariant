5a22313fd1fce61089d0f9a2f859e77916b1a703
ycy <ya70201@gmail.com>
2018-10-08 16:02:15 +0800

    init comment.

---
+++ baseline_model.py
@@ -0,0 +1,94 @@
+import torch
+from torch import nn
+import numpy as np
+
+
+def create_filters(d, k, sr=44100, start_freq=50, end_freq=6000):
+    x = torch.from_numpy(np.linspace(0, 2 * np.pi, d, endpoint=False).astype(np.float32))
+    num_cycles = start_freq * d / sr
+    scaling_ind = np.log(end_freq / start_freq) / k
+    window_mask = torch.hann_window(d) * 2
+
+    phases = torch.exp(torch.arange(float(k)) * scaling_ind)[None, :] * num_cycles * x[:, None]
+    filters = torch.stack((torch.cos(phases), torch.sin(phases)), 2) * window_mask[:, None, None]
+    return filters
+
+
+class spectrograms(nn.Module):
+    k = 512
+    d = 4096
+
+    d2_x = 1  # lvl2 input dims_x
+    d2_y = 128  # lvl2 input dims_y
+    k2 = 128  # num lvl2 filters
+    stride_y = 2  # lvl2 stride
+
+    d3_x = 25  # lvl3 input dims_x
+    d3_y = 1  # lvl3 input dims_y (fully connected)
+    k3 = 256  # num lvl3 filters
+
+    window_size = 16384
+    hop_size = 512
+    num_regions = 1 + (window_size - d) // hop_size  # 25
+    num_regions2_x = num_regions
+    num_regions2_y = 1 + (k - d2_y) // stride_y  # 193
+    num_regions3_x = 1 + (num_regions2_x - d3_x) // 1  # 1
+    num_regions3_y = num_regions2_y  # 193
+
+    m = 128
+
+    def __init__(self):
+        super().__init__()
+        # construct sin/cos weights
+        self.filter_banks = nn.Parameter(create_filters(self.d, self.k), requires_grad=False)
+
+        # need moving average?
+        self.w_w2 = nn.Sequential(nn.Conv2d(1, self.k2, (self.d2_x, self.d2_y), (1, self.stride_y), bias=False),
+                                  nn.ReLU(),
+                                  nn.Conv2d(self.k2, self.k3, (self.d3_x, self.d3_y), bias=False),
+                                  nn.ReLU())
+        self.beta = nn.Linear(self.num_regions3_x * self.num_regions3_y * self.k3, self.m, bias=False)
+
+    def forward(self, x):
+        # batch x 16384
+        x = x.unfold(1, self.d, self.hop_size) @ self.filter_banks.view(self.d, self.k * 2)
+        zx = x.view(-1, self.num_regions, self.k, 2).pow(2).sum(3).unsqueeze(1)
+        # batch x 1 x 25 x 512
+        z3 = self.w_w2(torch.log(zx+10e-15))
+        # batch x 256 x 1 x 193
+        y = self.beta(z3.view(-1, self.num_regions3_x * self.num_regions3_y * self.k3))
+        return y
+
+
+class EMA(nn.Module):
+    def __init__(self, mu):
+        super(EMA, self).__init__()
+        self.mu = mu
+        self.shadow = {}
+
+    def register(self, name, val):
+        self.shadow[name] = val.clone()
+
+    def forward(self, name, x):
+        assert name in self.shadow
+        new_average = self.mu * x + (1.0 - self.mu) * self.shadow[name]
+        self.shadow[name] = new_average.clone()
+        return new_average
+
+
+if __name__ == '__main__':
+    import librosa
+
+    y, sr = librosa.load(librosa.util.example_audio_file())
+    w = create_filters(1024, 128, sr)
+    y = torch.from_numpy(y).float()
+
+    y = y.unfold(0, 1024, 512) @ w.view(1024, 256)
+    y = y.view(-1, 128, 2).pow(2).sum(2)
+    y = y.detach().numpy().T
+    print(y.shape)
+
+    from matplotlib import pyplot as plt
+
+    plt.imshow(np.log1p(y), aspect='auto', origin='lower')
+    plt.show()
---
+++ train.py
@@ -0,0 +1,98 @@
+import torch
+from torch import optim
+from torch import nn
+import torch.backends.cudnn as cudnn
+from torch.utils.data import DataLoader
+import argparse
+
+from baseline_model import spectrograms, EMA
+import numpy as np
+from sklearn.metrics import average_precision_score, f1_score, precision_recall_fscore_support
+from musicnet import MusicNet
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--root', type=str, default='/host/data_dsk1/dataset/musicnet')
+parser.add_argument('--preprocess', action='store_true')
+parser.add_argument('--outfile', type=str, default='pre-trained.pth')
+parser.add_argument('--lr', type=float, default=0.00001)
+parser.add_argument('--steps', type=int, default=500000)
+parser.add_argument('--batch', type=int, default=150)
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    preprocess = args.preprocess
+    esize = args.steps * args.batch
+    batch_size = args.batch
+
+    window = 16384
+    decay = 2e-4
+
+    print('==> Loading Data...')
+    train_set = MusicNet(args.root, preprocess=args.preprocess, normalize=True, window=window, epoch_size=esize)
+    valid_set = MusicNet(args.root, preprocess=False, normalize=True, window=window, epoch_size=batch_size * 10)
+    train_loader = DataLoader(train_set, batch_size=batch_size, num_workers=4)
+    valid_loader = DataLoader(valid_set, batch_size=batch_size, num_workers=4)
+
+    print('==> Building model..')
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    net = spectrograms().to(device)
+
+    if torch.cuda.device_count() > 1:
+        net = nn.DataParallel(net)
+    if device == 'cuda':
+        cudnn.benchmark = True
+
+    print(sum(p.numel() for p in net.parameters() if p.requires_grad), "of parameters.")
+    ema = EMA(1 - decay)
+    for name, p in net.named_parameters():
+        if p.requires_grad:
+            ema.register(name, p.data)
+
+    criterion = nn.MSELoss(reduction='sum')
+    optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.95)
+
+    print("Start Training.")
+    global_step = 0
+    try:
+        with train_set, valid_set:
+            net.train()
+            for batch_idx, (inputs, targets) in enumerate(train_loader):
+                inputs, targets = inputs.to(device), targets.to(device)
+                optimizer.zero_grad()
+
+                outputs = net(inputs)
+                loss = criterion(outputs, targets) / 2 / inputs.size(0)
+                loss.backward()
+                optimizer.step()
+                for name, p in net.named_parameters():
+                    if p.requires_grad:
+                        p.data = ema(name, p.data)
+
+                global_step += 1
+
+                if global_step % 1000 == 0:
+                    net.eval()
+                    with torch.no_grad():
+                        y_true = []
+                        y_score = []
+                        for _, (inputs, targets) in enumerate(valid_loader):
+                            y_true += [targets.detach().numpy()]
+                            inputs = inputs.to(device)
+
+                            outputs = net(inputs)
+                            y_score += [outputs.detach().cpu().numpy()]
+
+                        y_score = np.vstack(y_score).flatten()
+                        y_true = np.vstack(y_true).flatten()
+                        print(global_step, loss.item(), average_precision_score(y_true, y_score))
+                    net.train()
+
+    except KeyboardInterrupt:
+        print('Graceful Exit')
+    else:
+        print('Finished')
+    net.eval()
+    net.cpu()
+    net = net.module if isinstance(net, torch.nn.DataParallel) else net
+    torch.save(net, args.outfile)
